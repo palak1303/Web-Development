3ce46674969c37567d357ab6375db4d7ed790459
palak1303 <palakrehan862@gmail.com>
2024-09-06 23:46:27 +0530

    DOM-wikiPageEg

---
+++ DOM/app.js
@@ -0,0 +1,50 @@
+// ----------class 9----------
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for( let i =0 ; i < smallImages.length ;i++){
+//     console.dir(smallImages[i].src);
+// }
+
+// how to access SELECTING
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelectorAll("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("div a"));
+
+// MANIPULATE
+
+// let links = document.querySelector(".box a");
+// // for(let i=0;i<links.length;i++){
+// //     links[i].style.color ="pink";
+// // }
+// for(link of links){
+//     link.style.color ="pink";
+// }
+
+// // Practice q
+// let para1 = document.createElement("p");
+// para1.innerText = "hey i  am new red";
+// document.querySelector('body').append(para1);
+
+// para1.classsList.add("red");
+
+
+// let h3 = document.createElement("h3");
+// h3.innerText = "hwy i  am new blue";
+// document.querySelector('body').append(h3);
+
+// h3.classsList.add("blue");
+
+// let div = document.createElement("div");
+// let h1 = document.createElement("h1")
+// let para2 = document.createElement("para2");
+// h1.innerText = "i'm in a div";
+// para2.innerHTML ="Mee Too!";
+// div.append(h1);
+// div.append(para2);
+// div.classList.add("box");
+
+// document.querySelector("body").append(div);
+
+
