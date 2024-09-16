9989e418fe40ca831fa26afd1ac4348843aab85e
palak1303 <palakrehan862@gmail.com>
2024-09-16 23:33:41 +0530

    BACKEND

---
+++ Backend/math.js
@@ -0,0 +1,12 @@
+const sum =(a,b)=> a+b;
+const sub =(a,b)=> a-b;
+const pi = 3.14;
+
+// module.export = "helli";
+
+let obj = {
+    sum : sum,
+    sub : sub,
+    pi : pi,
+};
+module.exportss = obj;
---
+++ Backend/script.js
@@ -0,0 +1,21 @@
+// ----Node js ----
+
+// Process 
+
+// let args = process.argv;
+// for(let i = 2; i<args.length;i++){
+//     console.log("hello",args[i]);   
+// }
+// let n = 5;
+// for(let i = 0; i <n ; i++){
+//     console.log("hello",i);
+// }
+
+// module.Exports
+// const some = require("./Fruit");
+// console.log(some);
+
+// import random
+
+import{ generate } from "random-words";
+ console.log(generate());
