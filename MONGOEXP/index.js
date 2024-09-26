bbc8e161b594139ca0b2e52cdebf6ff0fad6abac
palak1303 <palakrehan862@gmail.com>
2024-09-26 23:20:49 +0530

    MONGOEXP

--- MONGOEXP/index.js
+++ MONGOEXP/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError= require("./ExpressError.js");
 
 app.set("views" ,path.join(__dirname , "views"));
 app.set("views engine", "ejs ");
@@ -20,11 +21,13 @@ main()
 
 // new Route
 app.get("/chats/new" , (req , res)=>{
+    throw new ExpressError(404,"Page not found");
     res.render("new.ejs");
-})
+});
 
 // create route
-app.post("/chats" , (req,res)=>{
+app.post("/chats" ,async (req,res ,next)=>{
+    try{
     let {from ,to ,msg} = req.body; 
     let newChat = new Chat({
         from: from,
@@ -32,9 +35,7 @@ app.post("/chats" , (req,res)=>{
         msg: msg,
         created_at : new Date()
     });
-
-
-    newChat
+    await newChat
     .save()
     .then(res =>{
         console.log("chat was saved")
@@ -44,6 +45,9 @@ app.post("/chats" , (req,res)=>{
     });
 
     res.redirect("/chats");
+}catch(err){
+    next(err);
+}
 });
 
 app.get("/",(req,res)=>{
@@ -59,6 +63,24 @@ app.get("/chats" , async(req,res)=>{
     // console.log(chats);
     res.render("index.ejs" , {chats});
 });
+
+
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err) =>next(err));
+    };
+}
+
+// error handling
+// NEW Show Route
+app.get("/chats/:id" , asyncWrap(async(req,res,next) =>{
+    let {id} = req.params;
+    let chat =  await Chat.findById(id);
+    if(!chat){
+        return next( new ExpressError(404,"Chat Not Found"));
+    }
+    res.render("edit.ejs",{chat});
+}));
 
 // edit response
 app.get("/chats/:id/edit" , async (req,res) =>{
@@ -92,6 +114,25 @@ app.delete("/chats/:id" , async(req,res) =>{
 
 app.get("/" , (req,res)=>{
     res.send("root is working");
+});
+
+const handleValidationError = (err) =>{
+    console.log("This is a Validation Error .Please follow rules");
+    console.dir(err.message);
+    return err;
+}
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if( err.name ==="ValidationError"){
+        err = handleValidationError(err);
+    }
+    next(err);
+});
+
+// Middle ware
+app.use((err,req,res,next)=>{
+    let {status =500 ,message ="Some Error Occured"}=err;
+    res.status(status).send(message);
 })
 
 app.listen(8080 , ()=>{
