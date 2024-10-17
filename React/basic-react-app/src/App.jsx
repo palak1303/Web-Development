294fa1950dfd8d55de96534bd86fd14b5a43dade
palak1303 <palakrehan862@gmail.com>
2024-10-17 23:58:05 +0530

    REACT init

---
+++ React/basic-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Title from "./Title.jsx";
+import Product from "./Product.jsx";
+
+function App() {
+  return (
+    // <div>  
+    //   <Title/>
+    // </div>
+    <>
+    <Product/>
+    <Product/>
+    <Product/>
+    </>
+);
+
+} 
+
+export default App;
---
+++ React/basic-react-app/src/Title.jsx
@@ -0,0 +1,12 @@
+function Title(){
+    let name = "palak";
+    
+    return (<div>
+        <h1>I am the Title</h1>
+        <h1>I am the description</h1>
+        <p>hi,{name}</p>
+        <p>hi,{2*4}</p>
+    </div>
+  )}
+
+  export default Title;
