ecbc0f40c515a67c0b6b9164f5d1ba79539a6d6b
palak1303 <palakrehan862@gmail.com>
2024-10-27 21:01:54 +0530

    REACT(react-app)

---
+++ React/basic-react-app/src/Form.jsx
@@ -0,0 +1,10 @@
+function handleFormSubmit(){
+    console.log("form was submitted");
+}
+export default function Form(){
+    return (<form>
+        <input placeholder="write something"></input>
+        <button onClick ={handleFormSubmit}>Submit</button>
+        </form>
+    );
+}
